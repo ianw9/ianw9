0cb0283d8ec591f6f29de44682dbcb2587254528
ianw9 <82792337+ianw9@users.noreply.github.com>
2022-07-20 22:58:09 -0500

    Add files via upload

---
+++ dice homework test.c
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    srand(time(0));
+    while (i < 7) {
+            cout << i <<
+            i++;
+    int die1 = rand() % 6 + 1;
+    int die2 = rand() % 6 + 1;
+    cout << "die1: " << die1 << endl;
+    cout << "die2: " << die2 << endl;
+    if (die1, die2 % 6 == 7)
+        cout << die1, die2 << "you gain 7 points ";
+    else
+        cout << die1, die2 << "you must subtract 7 points to go back into the pot ";
+    if (die1, die2 == 21)
+        cout << die1, die2 << "you win ";
+    else
+        cout << die1, die2 << "you lose ";
+    return 0;
+}
